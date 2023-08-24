44bd4e5c6f71504b18cea81ddb06218b074065a0
pranavrao1 <pranavrao1@gmail.com>
2023-08-23 22:38:31 -0700

    Input checks for app

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+	"io/ioutil"
 )
+
+func ConfigExits() bool {
+	if _, err := os.Stat("conf.json"); err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func Dump() {
+	cred1 := Credential{alias: "Alias"}
+	fmt.Println("Running Credential", cred1)
+	fmt.Println(ConfigExits())
+	_ = ioutil.WriteFile("test.json", []byte(os.Args[0]), 0644)
+}
 
 type Credential struct {
 	alias string
@@ -12,7 +28,14 @@ type Credential struct {
 }
 
 func main() {
-	cred1 := Credential{alias: "Alias"}
-	fmt.Println("Running Crede", cred1)
-	fmt.Println(os.Args)
+
+	if !ConfigExits() {
+		fmt.Println("Please create a file called \"conf.json\" and enter the full path to store credentials")
+		return
+	} 
+
+	if len(os.Args) == 1 {
+		fmt.Println("No inputs provided.")
+		return
+	}
 }
